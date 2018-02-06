ac8d9b679ad470b1b757c2cc4c45978ad724728e
Philipp Paris <paris.philipp@gmail.com>
2018-02-06 18:06:41 +0100

    added MeasurementView

---
+++ app/src/main/java/com/philipp/paris/weatherapp/components/MeasurementView.java
@@ -0,0 +1,26 @@
+package com.philipp.paris.weatherapp.components;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.GridLayout;
+
+import com.philipp.paris.weatherapp.R;
+
+public class MeasurementView extends GridLayout {
+    public MeasurementView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public MeasurementView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.measurement_view, this, true);
+    }
+}
